a52b20c8f9c30d767f252a99cabb6627ae3759ff
amelitow <amel.tolba@outlook.fr>
2024-12-09 19:46:38 +0100

    Disposition des blocs et des espaces

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
-import { Button } from "kitchn";
+import { Button, Container, Text ,Spacer} from "kitchn";
 
 type WeatherData = {
   current: {
+    last_updated: string;
     temp_c: number;
     feelslike_c: number;
+    precip_in: number;
     condition: {
       text: string;
       icon: string;
@@ -43,29 +45,71 @@ const App: React.FC = () => {
   }, []); 
 
   return (
-    <div>
-      <h1>Météo à Paris</h1>
+    <Container>
+      <Text size={"extraTitle"} weight={"bold"} accent={"primary"}>Météo à Paris</Text>
+      <Spacer y={5} />
       {loading ? (
-        <p>Chargement des données...</p>
-      ) : error ? (
-        <p>Erreur : {error}</p>
-      ) : (
-        <div>
-          <p>Température : {weather?.current?.temp_c}°C</p>
-          <p>Ressenti : {weather?.current?.feelslike_c}°C </p>
-          <div>
-            <p>Description : {weather?.current?.condition?.text}</p>
-             <img src={`http:${weather?.current?.condition?.icon}`} alt="Icône météo" />
-          </div>
-          <p>Vent : {weather?.current?.wind_kph} km/h</p>
-          <p>Humidité : {weather?.current?.humidity} %</p>
-        </div>
+        <Text size={"medium"} accent={"light"}>Chargement des données...</Text>
+        ) : error ? (
+          <Text size={"medium"} accent={"dark"}>Erreur : {error}</Text>
+        ) : (
+
+      
+      <Container>
+
+        <Container row>
+          <Text size={"medium"} accent={"light"}>Date et Heure : {weather?.current?.last_updated} </Text>
+        </Container>
+      
+      <Spacer y={2} />
+      <Container row gap={10}>
+
+         <Container>
+            <Text>Température : {weather?.current?.temp_c}°C</Text>
+         </Container>
+
+         <Spacer x={5} />
+
+          <Container>
+            <Text>Ressenti : {weather?.current?.feelslike_c}°C </Text>
+          </Container>
+
+          <Spacer x={5} />
+
+        <Container gap={20}>
+
+          <Container>
+            <Text>Précipitation : {weather?.current?.precip_in} </Text>
+          </Container>
+          <Spacer y={2} />
+          <Container>
+            <Text>Vent : {weather?.current?.wind_kph} km/h</Text>
+          </Container>
+          <Spacer y={2} />
+         <Container>
+            <Text>Humidité : {weather?.current?.humidity} %</Text>
+          </Container>
+
+          <Spacer y={2} />
+
+        </Container>
+      </Container>
+        
+          
+        <Container row>
+          <Text>Description : {weather?.current?.condition?.text}</Text>
+          <Spacer x={5} />
+          <img src={`http:${weather?.current?.condition?.icon}`} alt="Icône météo" />
+        </Container>
+        
+      </Container>
       )}
 
-    <Button onClick={fetchWeather} variant="shadow" size="large" style={{ marginTop: "20px" }}>
+        <Button onClick={fetchWeather} variant="shadow" size="large" style={{ marginTop: "20px" }}>
         Rafraîchir
-      </Button>
-    </div>
+        </Button>
+
+    </Container>
 
   );
 
