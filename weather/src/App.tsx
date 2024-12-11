18be95a24ced097b5edaa4ba48e6b4e9d53e7fe1
amelitow <amel.tolba@outlook.fr>
2024-12-11 19:30:55 +0100

    ajout du style kitchn

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
-import { Button, Container, Text ,Spacer} from "kitchn";
+import { Button, Container, Text ,Spacer, Image, Scroller} from "kitchn";
+import { TiWeatherWindyCloudy } from "react-icons/ti";
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { FaWind } from "react-icons/fa";
+import { WiHumidity } from "react-icons/wi";
 
 type WeatherData = {
   current: {
@@ -46,70 +50,212 @@ const App: React.FC = () => {
 
   return (
     <Container>
-      <Text size={"extraTitle"} weight={"bold"} accent={"primary"}>Météo à Paris</Text>
-      <Spacer y={5} />
+
+      <Container 
+    justify={"center"}
+    align={"center"}
+    br={"square"}
+    bg={"rgb(23, 21, 40)"}
+    
+    >
+      <Container
+      justify={"center"}
+      marginTop={50}
+      >
+        <Text size={"extraTitle"} weight={"bold"} accent={"primary"}>Météo à Paris</Text>
+      </Container>
+      
+      <Spacer y={1} />
       {loading ? (
         <Text size={"medium"} accent={"light"}>Chargement des données...</Text>
         ) : error ? (
           <Text size={"medium"} accent={"dark"}>Erreur : {error}</Text>
         ) : (
-
+    <Container>
+    <Scroller width={"100%"} height={570}>
+      <Container
       
-      <Container>
+      padding={"40px"}
+      >
 
-        <Container row>
-          <Text size={"medium"} accent={"light"}>Date et Heure : {weather?.current?.last_updated} </Text>
+        <Container row
+        
+        bg={"rgb(251, 251, 251, 0.1)"}
+        padding={"40px"}
+        align={"center"}
+        br={"20px"}>
+          <Text 
+          size={"large"} 
+          accent={"light"}
+          
+          >Date et Heure </Text>
+          <Spacer x={2} />
+          <Text 
+          size={"large"} 
+          accent={"light"}
+          
+          >{weather?.current?.last_updated} </Text>
+        <Spacer x={5} />
+        <Button 
+        onClick={fetchWeather}
+        variant="shadow" 
+        size="large" 
+        >
+        Rafraîchir
+        </Button>
         </Container>
+        <Container 
+        row
+         >
+        
+      </Container>
       
       <Spacer y={2} />
-      <Container row gap={10}>
+      <Container 
+      row gap={10}
+      height={"450px"}
+      bg={"rgb(251, 251, 251, 0.1)"}
+      padding={"40px"}
+      br={"20px"}
+      >
 
-         <Container>
-            <Text>Température : {weather?.current?.temp_c}°C</Text>
+         <Container
+         bg={"rgb(251, 251, 251, 0.1)"}
+         padding={"40px"}
+         br={"20px"}
+         align={"center"}
+
+         >
+            <Text
+            size={"large"}
+            
+            >Température </Text>
+            <Spacer y={4}/>
+            <TbTemperatureCelsius
+            size={60}/>
+            <Spacer y={3}/>
+            <Text
+            size={"extraTitle"}
+            accent={"info"}
+            > {weather?.current?.temp_c}°C</Text>
          </Container>
 
-         <Spacer x={5} />
+         <Spacer x={1} />
 
-          <Container>
-            <Text>Ressenti : {weather?.current?.feelslike_c}°C </Text>
+          <Container
+          bg={"rgb(251, 251, 251, 0.1)"}
+          padding={"40px"}
+          br={"20px"}
+          align={"center"}>
+            <Text
+            size={"large"}>Ressenti  </Text>
+            <Spacer y={4}/>
+            <TiWeatherWindyCloudy 
+            size={60}/>
+            <Spacer y={3}/>
+            <Text
+            size={"extraTitle"}
+            accent={"info"}> {weather?.current?.feelslike_c}°C </Text>
           </Container>
 
-          <Spacer x={5} />
+          <Spacer x={1} />
 
-        <Container gap={20}>
+        <Container gap={20}
+        height={"450px"}>
 
-          <Container>
-            <Text>Précipitation : {weather?.current?.precip_in} </Text>
+          <Container
+          bg={"rgb(251, 251, 251, 0.1)"}
+          padding={"20px"}
+          br={"20px"}
+          align={"center"}>
+            <Text>Précipitation </Text>
+            <Spacer y={1.5}/>
+            <Text
+            size={"large"}
+            accent={"info"}> {weather?.current?.precip_in} </Text>
           </Container>
-          <Spacer y={2} />
-          <Container>
-            <Text>Vent : {weather?.current?.wind_kph} km/h</Text>
+          
+          <Container
+          bg={"rgb(251, 251, 251, 0.1)"}
+          padding={"20px"}
+          br={"20px"}
+          align={"center"}>
+            <Container
+            row
+            >
+            <FaWind 
+            size={20}/>
+            <Spacer x={1}/>
+              <Text>Vent </Text>
+            </Container>
+            
+            <Spacer y={1.5}/>
+            <Text
+            size={"large"}
+            accent={"info"}> {weather?.current?.wind_kph} km/h</Text>
           </Container>
-          <Spacer y={2} />
-         <Container>
-            <Text>Humidité : {weather?.current?.humidity} %</Text>
+          
+         <Container
+         bg={"rgb(251, 251, 251, 0.1)"}
+         padding={"20px"}
+         br={"20px"}
+         align={"center"}>
+          <Container
+          row>
+            <WiHumidity 
+            size={20}
+            />
+            <Spacer x={0.5}/>
+            <Text>Humidité </Text>
+          </Container>
+            
+            <Spacer y={1}/>
+            <Text
+            size={"large"}
+            accent={"info"}> {weather?.current?.humidity} %</Text>
           </Container>
 
-          <Spacer y={2} />
+          
 
         </Container>
       </Container>
         
           
-        <Container row>
-          <Text>Description : {weather?.current?.condition?.text}</Text>
-          <Spacer x={5} />
-          <img src={`http:${weather?.current?.condition?.icon}`} alt="Icône météo" />
+        <Container row
+        bg={"rgb(251, 251, 251, 0.1)"}
+        margin={"25px"}
+        padding={"40px"}
+        br={"20px"}
+        justify={"center"}>
+          <Container
+          align={"center"}>
+            <Text
+            size={"large"}>Description </Text>
+            <Spacer y={1}/>
+          <Text
+          size={"extraTitle"}> {weather?.current?.condition?.text}</Text>
+         
+          </Container>
+           <Spacer x={15} />
+          <Image 
+          src={`http:${weather?.current?.condition?.icon}`} 
+          alt="Icône météo"
+          width={"100px"}
+          height={"auto"}
+          />
         </Container>
         
       </Container>
+      </Scroller>
+      </Container>
       )}
 
-        <Button onClick={fetchWeather} variant="shadow" size="large" style={{ marginTop: "20px" }}>
-        Rafraîchir
-        </Button>
+      
+        
 
     </Container>
+    </Container>
+    
 
   );
 
